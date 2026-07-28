31a5230aeaf588dc2b76dffe6e6cc3d0c2285f0b
Agustin Tabarcache <agustintabarcache74@gmail.com>
2026-07-28 17:02:54 -0300

    fix: recreated a styles from original project

--- features/snippets/store/types.ts
+++ features/snippets/store/types.ts
@@ -9,3 +9,11 @@ export type Snippet = {
   createdAt: string;
   updatedAt: string;
 };
+
+export type SnippetFormData = {
+  title: string;
+  description: string;
+  language: string;
+  code: string;
+  tags: string;
+};
