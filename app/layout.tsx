32d3b416115f1cc90a32d72f78c160ef2aabce0e
Alvangie <alvarezluceroange@gmail.com>
2026-07-31 18:20:10 -0300

    feat(frente-5): update layout with Fira Code font and Open Graph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,32 @@ import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import "highlight.js/styles/github-dark.css";
 import "./globals.css";
+import { Fira_Code } from "next/font/google";
+
+const firaCode = Fira_Code({
+  subsets: ["latin"],
+  variable: "--font-fira-code",
+});
+
 
 export const metadata: Metadata = {
   title: "Biblioteca de Snippets",
   description:
     "Creá, listá, editá y eliminá tus snippets de código en un solo lugar.",
+     openGraph: {
+    title: "Biblioteca de Snippets",
+    description:
+      "Creá, listá, editá y eliminá tus snippets de código en un solo lugar.",
+    type: "website",
+    locale: "es_AR",
+  },
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
-    <html lang="es">
+    <html lang="es" className={firaCode.variable} >
       <body>{children}</body>
     </html>
   );
